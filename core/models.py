ef7d6f3fe184736fde811752413f4a08ee485814
PythonDecorator <okpeamos.ao@gmail.com>
2023-11-09 00:41:27 +0100

    Initial commit

---
+++ core/models.py
@@ -0,0 +1,77 @@
+"""
+Models for the CV App
+"""
+import uuid
+import os
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.conf import settings
+
+
+def image_file_path(instance, filename):  # noqa
+    """Generate file path for new image."""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+
+    return os.path.join('uploads', 'profile_images', filename)
+
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+
+        if not email:
+            raise ValueError("User must have an email address.")
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """Create, save and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=150, blank=True, null=True, verbose_name="First Name")
+    last_name = models.CharField(max_length=150, blank=True, null=True, verbose_name="Last Name")
+    date_joined = models.DateTimeField(default=timezone.now, verbose_name='date joined')
+    last_login = models.DateTimeField(blank=True, null=True, verbose_name='last login')
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+
+class Food(models.Model):
+
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=100)
+    carbs = models.FloatField()
+    protein = models.FloatField()
+    fats = models.FloatField()
+    calories = models.IntegerField()
+
+
+class Consume(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    food_consumed = models.ForeignKey(Food, on_delete=models.CASCADE)
